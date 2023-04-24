022c0eb205a78445a50817acb84a397dbc73653d
DeonaKoci <127995793+DeonaKoci@users.noreply.github.com>
2023-04-24 10:24:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+
+    int n=0;
+    cout << "inserire un numero intero n\n";
+    cin >> n;
+
+    int c=0,f,a=1,b=1;
+    if(n>=2){
+        cout << a << endl;
+        cout << b << endl;
+        while (c<(n-2)){
+            c++;
+            f=a+b;
+            cout << f << endl;
+            a=b;
+            b=f;
+        }
+
+    }else {
+        cout << "errore";
+    }
+
+    return 0;
 }
